6cc9e1a9422325049fe3167770fccf086d1a302f
regis hambalek <regis@hambalek.online>
2024-02-27 20:47:17 +0100

    progress adding computer objects

--- ad_tidy_inventory.ps1
+++ ad_tidy_inventory.ps1
@@ -60,6 +60,180 @@ Global:log -text ("Start V{0}" -F $Global:Version) -Hierarchy "Main"
 
 
 
+#region computers
+Global:ADTidy_Inventory_Computers_sql_table_check
+
+
+$last_update = Global_ADTidy_Iventory_Computers_last_update
+
+if ( ([string]$last_update.maxrecord).Length -eq 0 ) {
+    $filter = "*"
+}
+else {
+    
+    $filter_date = get-date $last_update.maxrecord  | ForEach-Object touniversaltime | get-date -format yyyyMMddHHmmss.0Z
+    $filter = "whenchanged -gt '$filter_date'"
+}
+Global:log -text ("retrieving computers from AD, filter='{0}'" -F $filter) -Hierarchy "Main:Users"
+
+$computers = @()
+
+#region delta changes
+<# PRD#>
+Get-ADComputer -properties $global:config.Configurations.inventory.'Computers Active Directory Attributes' -filter $filter  | ForEach-Object { 
+    <# DEV 
+Get-ADUser  -properties $global:config.Configurations.inventory.'Active Directory Attributes' -filter "samaccountname -eq 'har3005'"  | ForEach-Object { #> 
+
+
+    $this_row = $_
+    $this_calculated_row = "" | Select-Object ignore
+
+    $this_row | Get-Member | Where-Object { $_.membertype -eq "property" } | Select-Object -ExpandProperty name | ForEach-Object {
+        $this_attribute = $_
+        $this_calculated_row = $this_calculated_row | Select-Object *, $this_attribute
+        Switch ( $this_attribute ) {
+            "accountexpires" {
+                $AccountExpiresRaw = [string]($this_row."$this_attribute")
+                if ( $AccountExpiresRaw -eq "9223372036854775807" ) { 
+                    $AccountExpires = $null
+                }
+                else {
+                    $AccountExpires = [datetime]::FromFileTime($AccountExpiresRaw).ToString("yyyy-MM-dd HH:mm:ss") 
+                    if ( $AccountExpires -eq '1601-01-01 01:00:00' ) { $AccountExpires = $null }
+                }
+                if ( $AccountExpires.Length -eq 0 ) { $CalulatedValue = "NULL" }
+                ELSE { $CalulatedValue = "$AccountExpires" }
+                           
+                $this_calculated_row."$this_attribute" = $CalulatedValue
+
+            }
+            "lastLogonTimestamp" {
+                $AccountExpiresRaw = [string]($this_row."$this_attribute")
+                if ( $AccountExpiresRaw -eq "9223372036854775807" ) { 
+                    $AccountExpires = $null
+                }
+                else {
+                    $AccountExpires = [datetime]::FromFileTime($AccountExpiresRaw).ToString("yyyy-MM-dd HH:mm:ss") 
+                    if ( $AccountExpires -eq '1601-01-01 01:00:00' ) { $AccountExpires = $null }
+                }
+                if ( $AccountExpires.Length -eq 0 ) { $CalulatedValue = "NULL" }
+                ELSE { $CalulatedValue = "$AccountExpires" }
+                           
+                $this_calculated_row."$this_attribute" = $CalulatedValue
+
+            }
+            "pwdLastSet" {
+                $pwdLastSetRaw = [string]($this_row."$this_attribute")
+                if ( $pwdLastSetRaw -eq "9223372036854775807" ) { 
+                    $pwdLastSet = $null
+                }
+                else {
+                    $pwdLastSet = [datetime]::FromFileTime($pwdLastSetRaw).ToString("yyyy-MM-dd HH:mm:ss") 
+                    if ( $pwdLastSet -eq '1601-01-01 01:00:00' ) { $pwdLastSet = $null }
+                }
+                if ( $pwdLastSet.Length -eq 0 ) { $CalulatedValue = "NULL" }
+                ELSE { $CalulatedValue = "$pwdLastSet" }
+                           
+
+                $this_calculated_row."$this_attribute" = $CalulatedValue
+                
+            }
+            "useraccountcontrol" {
+                #write-host "> useraccountcontrol"
+                $CalulatedValue = Global:DecodeUserAccountControl ([int][string]($this_row."$this_attribute"))
+                #$CalulatedValue = "'$CalulatedValue'"
+                $this_calculated_row."$this_attribute" = $CalulatedValue
+            }
+            "thumbnailPhoto" {
+                if ( 0) {
+                    #Write-Host "thumbnailPhoto"
+                            
+                    if ( $ThisFieldValue -ne $null) {
+                        $CalulatedValue = "Is set"
+                    }
+                    else {
+                        $CalulatedValue = "NULL"
+                    }
+                    #Write-Host "value:$ThisFieldValue"
+                }
+            }
+            "whenChanged" {
+                TRY {
+                    $CalulatedValue = '{0:yyyy-MM-dd HH:mm:ss}' -f $this_row."$this_attribute"
+                    $this_calculated_row."$this_attribute" = $CalulatedValue 
+                }
+                CATCH {
+                    $this_calculated_row."$this_attribute" = $null
+                }
+                #write-host ( "   > {0} - {1}" -F ($this_row."$this_attribute"), $this_attribute)
+                
+                
+            }
+            "whenCreated" {
+                TRY {
+                    $CalulatedValue = '{0:yyyy-MM-dd HH:mm:ss}' -f $this_row."$this_attribute"
+                    $this_calculated_row."$this_attribute" = $CalulatedValue 
+                }
+                CATCH {
+                    $this_calculated_row."$this_attribute" = $null
+                }
+                #write-host ( "   > {0} - {1}" -F ($this_row."$this_attribute"), $this_attribute)
+                
+            }
+            default {
+                $this_calculated_row."$this_attribute" = $this_row."$this_attribute" -replace "'", "''" 
+            }
+
+        }
+    }
+    
+    $this_calculated_row = ($this_calculated_row | Select-Object * -ExcludeProperty ignore, enabled)
+    $computers += $this_calculated_row
+
+}
+
+if ( $computers.Count -eq 0 ) {
+    Global:log -text ("No  computers objects changes found" -F $computers.Count) -Hierarchy "Main:Ou:delta changes" -type warning
+}
+else {
+    Global:log -text ("{0} computers objects retrieved" -F $computers.Count) -Hierarchy "Main:Users:delta changes"
+
+    $computers | Select-Object * -ExcludeProperty  objectclass, DNSHostName, UserPrincipalName | ForEach-Object {
+        $this_computer = $_
+        Global:ADTidy_Inventory_Computers_sql_update -Fields $this_computer
+    }
+
+}
+exit
+#endregion
+
+#region deleted records detect
+$sql_current_records = Global_ADTidy_Iventory_Users_all_current_records 
+$ad_current_records = Get-ADUser -filter * -Properties objectguid | Select-Object -ExpandProperty objectguid
+Global:log -text ("SQL:{0} current records, AD:{1} current records " -F $sql_current_records.Count, $ad_current_records.Count) -Hierarchy "Main:Users:deleted detect"
+$flag_deleted = 0
+
+$sql_current_records | ForEach-Object {
+    $this_sql_record = $_
+    if ( $ad_current_records -notcontains $this_sql_record.ad_objectguid) {
+        Global:log -text ("Detected a deleted AD account:'{0}' " -F ($this_sql_record | Select-Object ad_samaccountname, ad_objectguid, ad_sid | ConvertTo-Json -Compress)) -Hierarchy "Main:Users:deleted detect"
+        $delete_record = $this_sql_record | Select-Object @{name = 'Objectguid'; expression = { $_.ad_ObjectGUID } }, record_status
+        $delete_record.record_status = "Deleted"
+        Global:ADTidy_Inventory_Users_sql_update -Fields $delete_record
+        $flag_deleted = 1
+    }
+}
+if ( $flag_deleted -eq 0) {
+    Global:log -text ("No deleted user record found." ) -Hierarchy "Main:Users:deleted detect" -type warning
+
+}
+#endregion
+
+#endregion
+
+
+exit
+
 #region OU
 Global:ADTidy_Inventory_OU_sql_table_check
 
@@ -302,7 +476,7 @@ Get-ADUser  -properties $global:config.Configurations.inventory.'Active Director
 }
 
 if ( $users.Count -eq 0 ) {
-    Global:log -text ("No Ou users objects changes found" -F $Organizational_units.Count) -Hierarchy "Main:Ou:delta changes" -type warning
+    Global:log -text ("No  user objects changes found" -F $users.Count) -Hierarchy "Main:Ou:delta changes" -type warning
 }
 else {
     Global:log -text ("{0} user objects retrieved" -F $users.Count) -Hierarchy "Main:Users:delta changes"
@@ -339,7 +513,6 @@ if ( $flag_deleted -eq 0) {
 #endregion
 
 #endregion
-
 
 #region Groups
 Global:ADTidy_Inventory_Groups_sql_table_check
--- includes/sql.ps1
+++ includes/sql.ps1
@@ -932,3 +932,289 @@ function Global_ADTidy_Iventory_Groups_all_current_records {
 
     return Global:sql_query -query ("SELECT [ad_name],[ad_objectguid],[ad_distinguishedname] FROM [ittool].[dbo].[{0}] WHERE [record_status] = 'Current'" -F $Table_Name )
 }
+
+
+function Global:ADTidy_Inventory_Computers_sql_table_check {
+    param(
+        $Table_Name = "ADTidy_Inventory_Computers"
+    )
+
+    $config = @"
+{"Fields": [{"name": "record_source","type": "VARCHAR(100)"},{"name": "record_lastupdate","type": "DATETIME"},{"name": "ad_lastlogontimestamp","type": "DATETIME","nullable": 1},{"name": "record_status","type": "VARCHAR(50)"},{"name": "ad_whenCreated","type": "DATETIME"},{"name": "ad_whenChanged","type": "DATETIME"},{"name": "ad_objectguid","type": "VARCHAR(50)"},{"name": "ad_samaccountname","type": "VARCHAR(50)"},{"name": "ad_name","type": "VARCHAR(100)"},{"name": "ad_distinguishedname","type": "VARCHAR(MAX)"},{"name": "ad_description","type": "VARCHAR(MAX)","nullable": 1},{"name": "ad_info","type": "VARCHAR(MAX)","nullable": 1},{"name": "ad_managedBy","type": "VARCHAR(MAX)","nullable": 1},{"name": "ad_operatingSystem","type": "VARCHAR(100)","nullable": 1},{"name": "ad_operatingSystemVersion","type": "VARCHAR(50)","nullable": 1},{"name": "ad_extensionattribute1","type": "VARCHAR(50)","nullable": 1},{"name": "ad_extensionattribute2","type": "VARCHAR(50)","nullable": 1},{"name": "ad_extensionattribute3","type": "VARCHAR(50)","nullable": 1},{"name": "ad_extensionattribute4","type": "VARCHAR(50)","nullable": 1},{"name": "ad_sid","type": "VARCHAR(50)"}],"FieldsAssignement": [{"name": "record_source","Recipe":[{"ORDER":"1", "Content": [ {"Source": "Calculation","Type": "record_source"}]}]},{"name": "record_lastupdate","Recipe":[{"ORDER":"1", "Content": [ {"Source": "Calculation","Type": "current_datetime"}]}]},{"name": "ad_lastlogontimestamp","Recipe":[{"ORDER":"1", "Content": [ {"Source": "Field","Type": "lastlogontimestamp"}]}]},{"name": "record_status","Recipe":[{"ORDER":"1", "Content": [ {"Source": "Field","Type": "status"}]}]},{"name": "ad_whenCreated","Recipe":[{"ORDER":"1", "Content": [ {"Source": "Field","Type": "whenCreated"}]}]},{"name": "ad_whenChanged","Recipe":[{"ORDER":"1", "Content": [ {"Source": "Field","Type": "whenChanged"}]}]},{"name": "ad_objectguid","Recipe":[{"ORDER":"1", "Content": [ {"Source": "Field","Type": "objectguid"}]}]},{"name": "ad_samaccountname","Recipe":[{"ORDER":"1", "Content": [ {"Source": "Field","Type": "samaccountname"}]}]},{"name": "ad_name","Recipe":[{"ORDER":"1", "Content": [ {"Source": "Field","Type": "name"}]}]},{"name": "ad_distinguishedname","Recipe":[{"ORDER":"1", "Content": [ {"Source": "Field","Type": "distinguishedname"}]}]},{"name": "ad_description","Recipe":[{"ORDER":"1", "Content": [ {"Source": "Field","Type": "description"}]}]},{"name": "ad_info","Recipe":[{"ORDER":"1", "Content": [ {"Source": "Field","Type": "info"}]}]},{"name": "ad_managedBy","Recipe":[{"ORDER":"1", "Content": [ {"Source": "Field","Type": "managedBy"}]}]},{"name": "ad_operatingSystem","Recipe":[{"ORDER":"1", "Content": [ {"Source": "Field","Type": "os1"}]}]},{"name": "ad_operatingSystemVersion","Recipe":[{"ORDER":"1", "Content": [ {"Source": "Field","Type": "os2"}]}]},{"name": "ad_extensionattribute1","Recipe":[{"ORDER":"1", "Content": [ {"Source": "Field","Type": "ea1"}]}]},{"name": "ad_extensionattribute2","Recipe":[{"ORDER":"1", "Content": [ {"Source": "Field","Type": "ea2"}]}]},{"name": "ad_extensionattribute3","Recipe":[{"ORDER":"1", "Content": [ {"Source": "Field","Type": "ea3"}]}]},{"name": "ad_extensionattribute4","Recipe":[{"ORDER":"1", "Content": [ {"Source": "Field","Type": "ea4"}]}]},{"name": "ad_sid","Recipe":[{"ORDER":"1", "Content": [ {"Source": "Field","Type": "sid"}]}]}]}										
+"@ | ConvertFrom-Json
+
+
+    Global:Log -Hierarchy ("function:{0}" -F $MyInvocation.MyCommand ) -text ("Running check for table:{0}" -F $Table_Name) -type warning
+    $Query_Check_Table_Exists = "SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = '{0}'" -F $Table_Name
+    $Result_Check_Table_Exists = Global:sql_query -query $Query_Check_Table_Exists
+
+    if ( ($Result_Check_Table_Exists | Measure-Object).count -eq 0 ) {
+        Global:Log -Hierarchy ("function:{0}" -F $MyInvocation.MyCommand ) -text ("table '{0}' does not exist" -F $Table_Name) -type warning
+        $Query_Create_Table_main = "CREATE TABLE {0} ({1}{2});"
+        $Query_Create_Table_Constraint = "CONSTRAINT {0} UNIQUE ({1})" 
+        
+        $Query_String_Fields = ""
+        $Query_String_Constraints = ""
+        $Has_Contraints = 0
+        $config.Fields | ForEach-Object {
+            if ($_.nullable -ne 1) { $string_nullable = " NOT NULL" } else { $string_nullable = " NULL" }
+            if ($_.id -eq 1) { $string_id = " IDENTITY(1,1) NOT NULL"; $string_nullable = $null } else { $string_id = $null }
+            $Query_String_Fields += "`n{0} {1}{2}{3}," -F $_.name, $_.type, $string_id, $string_nullable
+            if ($_.constraints -eq 1) { $Query_String_Constraints += "{0}," -F $_.Name; $Has_Contraints = 1 }
+            
+    
+        }
+        $Query_String_Fields = $Query_String_Fields.Substring(1)# removes first character  from string
+        if ( $Has_Contraints -eq 0 ) {
+            #write-host "no constraints"
+            $Query_String_Fields = $Query_String_Fields -replace ".$" # removes last character from string
+            $Query_Final_Create_Query = $Query_Create_Table_main -F $Table_Name, $Query_String_Fields, $null
+        }
+        else {
+            #write-host "with constraints"
+            $Query_String_Constraints = $Query_String_Constraints -replace ".$" # removes last character from string
+            $Query_String_Constraints = $Query_Create_Table_Constraint -F ("CONSTRAINT_{0}" -F $Table_Name), $Query_String_Constraints
+            $Query_Final_Create_Query = $Query_Create_Table_main -F $Table_Name, ("`n" + $Query_String_Fields), ("`n" + $Query_String_Constraints)
+        }
+        IF ( $Global:WhatIf -ne $true ) {
+            Global:sql_query -query $Query_Final_Create_Query 
+        }
+        ELSE {
+            Global:Log -Hierarchy ("function:{0}" -F $MyInvocation.MyCommand ) -text ("{0}" -F $Query_Final_Create_Query ) -type warning
+            Global:Log -Hierarchy ("function:{0}" -F $MyInvocation.MyCommand ) -text ("WHATIF=`$true") -type error
+        }
+
+
+    }
+    else {
+        $Table_schema_name = "[{0}].[{1}].[{2}]" -F $Result_Check_Table_Exists.TABLE_CATALOG, $Result_Check_Table_Exists.TABLE_SCHEMA, $Result_Check_Table_Exists.TABLE_NAME
+        
+        $Query_Select_all = "SELECT * FROM {0}" -F $Table_Name
+        Global:Log -Hierarchy ("function:{0}" -F $MyInvocation.MyCommand ) -text ("table '{0}' exists, {1} rows in it" -F $Table_Name, (Global:sql_query -query $Query_Select_all).count) 
+        
+        Global:Log -Hierarchy ("function:{0}" -F $MyInvocation.MyCommand ) -text "Verifiying columns..."
+        $Query_List_columns = "SELECT col.name AS column_name,t.name AS data_type,col.max_length AS data_type_detail FROM sys.tables AS tab INNER JOIN sys.columns AS col ON tab.object_id = col.object_id LEFT JOIN sys.types AS t ON col.user_type_id = t.user_type_id WHERE tab.name = '{0}'" -F $Table_Name
+        $SqlCurrentColumns = Global:sql_query -query $Query_List_columns
+
+        $Queries_Update_table = ""
+        $config.Fields | ForEach-Object {
+            $this = $_ | Select-Object *, type_sql, type_detail
+
+            #write-host ( "split of {0}, count:{1}" -F $this.type, ($this.type).split("(").count )
+            if ( ($this.type).split("(").count -eq 1) {
+                # no detail type such as DATETIME or INT
+                $this.type_sql = ($this.type).split("(")[0]
+                $this.type_detail = $null
+            }
+            else {
+                $this.type_sql = ($this.type).split("(")[0]
+                $this.type_detail = ($this.type).split("(")[1] -replace ".$"
+            }
+            #write-host ( "json this:{0}" -F ( $this | ConvertTo-Json -Compress ))
+            
+            $MatchingColumn = "" | Select-Object found, same_type, type_details_match
+            $MatchingColumn.found = 0
+            $MatchingColumn.same_type = 0
+            $MatchingColumn.type_details_match = 0
+            $SqlCurrentColumns | Where-Object { $_.column_name -eq $this.name } | ForEach-Object {
+                $MatchingColumn.found = 1
+                if ( $_.data_type -eq $this.type_sql ) { 
+                    $MatchingColumn.same_type = 1
+                    if ($_.data_type_detail -eq -1) { $temp_data_type_detail = [string]"MAX" } ELSE { $temp_data_type_detail = [string]$_.data_type_detail }
+                    if ( $this.type_detail -ne $null) {
+                        if ( $temp_data_type_detail -eq $this.type_detail) {
+                            $MatchingColumn.type_details_match = 1
+                        }
+                        else {
+                            Global:Log -Hierarchy ("function:{0}" -F $MyInvocation.MyCommand ) -text ( "Missmatching type detail for column ({3}) {2} :({0} <> {1})." -F $this.type_detail, $temp_data_type_detail, $this.name, $this.type) -type warning
+                        }
+                    }
+                    else {
+                        $MatchingColumn.type_details_match = 1
+                    }
+
+                }
+                else {
+                    Global:Log -Hierarchy ("function:{0}" -F $MyInvocation.MyCommand ) -text ( "Missmatching type for column {2} ({0} <> {1})." -F $this.type_sql, $_.data_type, $this.name) -type warning
+                }
+                #write-host ( "json MatchingColumn:{0}" -F ( $MatchingColumn | ConvertTo-Json -Compress ))
+
+            }
+        
+
+            if ( $MatchingColumn.found -eq 0 ) {  
+                Global:Log -Hierarchy ("function:{0}" -F $MyInvocation.MyCommand ) -text ( "column {0} missing" -F $this.name) -type warning
+
+            }
+
+            $Query_Run = $null
+            $Query_Alter_Table_Add_Column = "ALTER TABLE {0} ADD {1} {2};"
+            $Query_Alter_Table_Alter_Column = "ALTER TABLE {0} ALTER COLUMN {1} {2};"
+
+            if ( $MatchingColumn.found -eq 0 ) { 
+                $Query_Run = $Query_Alter_Table_Add_Column 
+            }
+            else {
+                if ( $MatchingColumn.same_type -eq 0 -or $MatchingColumn.type_details_match -eq 0 ) {
+                    $Query_Run = $Query_Alter_Table_Alter_Column 
+                }
+            }
+
+            if ( $Query_Run -ne $null ) {
+                $Query_Run = $Query_Run -F $Table_schema_name, $this.name, $this.type
+                $Queries_Update_table += $Query_Run + "`n"
+            }
+
+        }
+
+        if ( $Queries_Update_table.Length -ge 1 ) {
+            Global:Log -Hierarchy ("function:{0}" -F $MyInvocation.MyCommand ) -text ( "resulting alter table queries:{0}" -F $Queries_Update_table)
+            IF ( $Global:WhatIf -ne $true ) {
+                Global:sql_query -query $Queries_Update_table 
+            }
+            ELSE {
+                Global:Log -Hierarchy ("function:{0}" -F $MyInvocation.MyCommand ) -text ("{0}" -F $Queries_Update_table ) -type warning
+                Global:Log -Hierarchy ("function:{0}" -F $MyInvocation.MyCommand ) -text ("WHATIF=`$true") -type error
+            }
+
+        }
+        else {
+            Global:Log -Hierarchy ("function:{0}" -F $MyInvocation.MyCommand ) -text ( "Table {0} is matching the definition of the config.json at run time." -F $Table_Name)
+        }
+
+        
+    }
+}
+function Global_ADTidy_Iventory_Computers_last_update {
+    param(
+        $Table_Name = "ADTidy_Inventory_Computers"
+    )
+    Global:log -Hierarchy ("function:{0}" -F $MyInvocation.MyCommand ) -text ("Retrieving max whenchanged timestamp..." )
+
+    return Global:sql_query -query ("SELECT max(ad_whenchanged) as maxrecord  FROM [{0}]" -F $Table_Name )
+}
+function Global:ADTidy_Inventory_Computers_sql_update {
+    param(
+        [Parameter(Mandatory = $true)] [array]$Fields,
+        $Table_Name = "ADTidy_Inventory_Computers"
+    )
+
+    #region ADTidy_Inventory_Users_sql_update specific
+    $prefixed_fields = "" | Select-Object ignore
+    $varchar_field = @()
+    $ObjectGUID = $Fields.ObjectGUID
+    $Fields | Select-Object -ExcludeProperty ObjectGUID | Get-Member | Where-Object { $_.membertype -eq "NoteProperty" } | Select-Object -ExpandProperty name | ForEach-Object {
+        $this_attribute_name = $_
+        switch ($this_attribute_name) {
+            "record_status" { $prefixed_attribute_name = $this_attribute_name }
+            "xml_members" { $prefixed_attribute_name = $this_attribute_name }
+            default { $prefixed_attribute_name = "ad_{0}" -F $this_attribute_name }
+
+        }
+        
+        $varchar_field += $prefixed_attribute_name
+        $prefixed_fields = $prefixed_fields | Select-Object *, $prefixed_attribute_name
+        $prefixed_fields."$prefixed_attribute_name" = $Fields."$this_attribute_name"
+
+    }
+    $Fields = $prefixed_fields
+    Global:log -Hierarchy ("function:{0}" -F $MyInvocation.MyCommand )  -text ( "prefixed_fields:{0}" -F $prefixed_fields | ConvertTo-Json -Compress    )
+
+    #endregion
+
+    #region function internal definitions
+    $table = $Table_Name
+    #$sql_varchar_fields = @("entry_type", "entry_details", "user_type", "user_employeeid", "user_samaccountname", "user_guid", "execution_status_details", "execution_mode", "execution_operator_name", "execution_operator_action_timestamp", "execution_logs", "execution_last_update", "record_status")
+    #$sql_special_fields = @("entry_repeat", "entry_first_occurrence", "entry_last_occurrence", "execution_status_code")
+    #endregion
+
+    #region create INSERT statement field and value pairs, loop throuhg $Fields array matching sql_field definition
+    $sql_statement_fields = ""
+    $sql_statement_values = ""
+    $sql_update_statement = "$sql_update_statement"
+    $varchar_field | ForEach-Object {
+        $thisField = $_
+        if ( $Fields."$thisField" -ne $null ) {
+            #Global:Log -text (" + `$Fields contains attribute '{0}'" -F $thisField) -hierarchy "function:adimport_sql_update:DEBUG"
+            $sql_statement_fields = "{0} [{1}]," -F $sql_statement_fields, $thisField
+            if ($Fields."$thisField" -eq "NULL") {
+                $sql_statement_values = "{0} {1}," -F $sql_statement_values, $Fields."$thisField"
+                $sql_update_statement = "{0} [{1}]={2}," -F $sql_update_statement, $thisField, $Fields."$thisField"
+            }
+            else {
+                $sql_statement_values = "{0} '{1}'," -F $sql_statement_values, $Fields."$thisField"
+                $sql_update_statement = "{0} [{1}]='{2}'," -F $sql_update_statement, $thisField, $Fields."$thisField"
+            }
+            
+            
+        }
+        else {
+            #Global:Log -text (" ! `$Fields misses attribute '{0}'" -F $thisField) -hierarchy "function:adimport_sql_update:DEBUG"
+        }
+    }
+    #endregion
+
+    #region check if this user ( guid based ) exists 
+    $exist_query = "select * FROM {0} WHERE ad_objectguid ='{1}'" -F $Table_Name, $Fields.ad_objectguid
+    $current_record = Global:sql_query -query $exist_query
+    if ( $current_record.count -ne 0) {
+        $action_type = "update"
+    }
+    else {
+        $action_type = "new"
+    }
+    #endregion
+
+    
+    #region new record
+    if ( $action_type -eq "new") {
+        Global:log -Hierarchy ("function:{0}" -F $MyInvocation.MyCommand )  -text ( "action_type = new"  )
+
+
+        $sql_statement_fields = "{0} [record_source]," -F $sql_statement_fields
+        $sql_statement_values = "{0} 'ADTidy'," -F $sql_statement_values
+        $sql_statement_fields = "{0} [record_status]," -F $sql_statement_fields
+        $sql_statement_values = "{0} 'Current'," -F $sql_statement_values
+        $sql_statement_fields = "{0} [record_lastupdate]," -F $sql_statement_fields
+        $sql_statement_values = "{0} GETDATE()," -F $sql_statement_values
+        
+        # remove last ',' from both fields and values strings
+        $sql_statement_fields = $sql_statement_fields -replace ".$"
+        $sql_statement_values = $sql_statement_values -replace ".$"
+
+        $sql_statement_insert = " INSERT INTO {0} ({1}) VALUES ({2})" -F $table, $sql_statement_fields, $sql_statement_values
+        
+        Global:log -Hierarchy ("function:{0}" -F $MyInvocation.MyCommand )  -text ( "query:'{0}'" -F $sql_statement_insert  )
+        $insert_result = Global:sql_query -query $sql_statement_insert
+
+
+        #Global:Log -text ("Inserted row id: '{0}' " -F $insert_result.rowid) -hierarchy "function:adimport_sql_update:DEBUG"
+        #return $insert_result.rowid
+    }
+    #endregion
+
+
+    #region update record
+    if ( $action_type -eq "update" ) {
+        Global:log -Hierarchy ("function:{0}" -F $MyInvocation.MyCommand )  -text ( "action_type = update"  )
+
+        $sql_update_filter = " [ad_ObjectGUID] = '{0}'" -F $ObjectGUID
+
+        $sql_update_statement = "{0} [record_lastupdate] = GETDATE()," -F $sql_update_statement
+
+        # remove last ',' from string
+        $sql_update_statement = $sql_update_statement -replace ".$"
+
+        $sql_statement_update = " UPDATE {0} SET {1} WHERE {2}" -F $table, $sql_update_statement, $sql_update_filter
+        #Global:log -Hierarchy ("function:{0}" -F $MyInvocation.MyCommand )  -text ( "query:'{0}'" -F $sql_statement_update  )
+        Global:sql_query -query $sql_statement_update
+        return $Fields.ObjectGUID
+    }
+    #endregion
+
+
+
+
+}
+
+										
